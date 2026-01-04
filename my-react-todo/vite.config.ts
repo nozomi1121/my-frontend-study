ae944d3653bddacd44c7c7cb9b9304eb54642ea8
nozomi1121 <smithnozomu@gmail.com>
2026-01-05 03:15:53 +0900

    GitHub公開_Publicに変更後

--- my-react-todo/vite.config.ts
+++ my-react-todo/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // リポジトリ名（my-react-todo）を / で囲んで書きます
-  base: '/my-react-todo/', 
+  // リポジトリ名を「my-react-todo」→から「/my-frontend-study/」に変更
+  base: '/my-frontend-study/', 
 })
